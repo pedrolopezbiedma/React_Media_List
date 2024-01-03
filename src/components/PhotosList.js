fef432fa7a18e9fdea13c00671283a06d68ab372
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-03 23:45:52 +0100

    Creating new photos

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,10 +1,25 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, usePostPhotoMutation } from "../store";
+import Button from "./Button";
 
 const PhotosList = ({ album }) => {
   const { data, error, isFetching } = useFetchPhotosQuery(album);
+  const [addPhoto, results] = usePostPhotoMutation();
   console.log("Photos for the album are -->", data);
 
-  return <h1>This is the PhotosList</h1>;
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos for {album.title}</h3>
+        <Button loading={false} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default PhotosList;
